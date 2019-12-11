d80e7e0e57bf1bc5f0a06db0c63a27d4d8cb7067
ram-9047 <ramg786@live.com>
2019-12-11 18:30:26 +0530

    checkout page styling done

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -16,37 +16,47 @@ class Checkout extends React.Component {
   render() {
     return (
       <>
-        <h4>Checkout</h4>
-        <form>
-          <label>
-            Name:
-            <input
-              type="text"
-              name="name"
-              value={this.state.name}
-              onChange={this.handleChange}
-            />
-          </label>
-          <label>
-            Email:
-            <input
-              type="email"
-              name="email"
-              value={this.state.email}
-              onChange={this.handleChange}
-            />
-          </label>
-          <label>
-            Phone:
-            <input
-              type="number"
-              name="phone"
-              value={this.state.phone}
-              onChange={this.handleChange}
-            />
-          </label>
+        <h4 className="carttitle">Checkout</h4>
+        <form className="form">
+          <div>
+            <label className="form-item">
+              Name:
+              <input
+                type="text"
+                name="name"
+                value={this.state.name}
+                onChange={this.handleChange}
+                className="form-input"
+              />
+            </label>
+          </div>
+          <div>
+            <label className="form-item">
+              Email:
+              <input
+                type="email"
+                name="email"
+                value={this.state.email}
+                onChange={this.handleChange}
+                className="form-input"
+              />
+            </label>
+          </div>
+          <div>
+            <label className="form-item">
+              Phone:
+              <input
+                type="number"
+                name="phone"
+                value={this.state.phone}
+                onChange={this.handleChange}
+                className="form-input"
+              />
+            </label>
+          </div>
           <input
             type="submit"
+            className="checkout-btn submit-btn"
             value="Submit"
             onClick={() =>
               alert(`${this.state.name} already bought these itmes.`)
